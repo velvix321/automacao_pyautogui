7bca3543d57aa0d9c806ae0402a65bcdaa16b3d1
velvix321 <velvix321@gmail.com>
2025-08-01 12:09:53 -0300

    committed

--- teste4selenium/index.py
+++ teste4selenium/index.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 navegador = webdriver.Chrome()
-
 navegador.get("https://google.com")
+
+caixa_busca = navegador.find_element(By.NAME, "q")
+caixa_busca.send_keys("python selenium")
+caixa_busca.send_keys(Keys.ENTER)
+
+time.sleep(5)
+navegador.quit()
